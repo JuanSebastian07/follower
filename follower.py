d8cb167fe54add5eaa8190d3d2a119dff6832ab3
JuanSebastian07 <93341765+JuanSebastian07@users.noreply.github.com>
2022-02-13 21:33:46 -0500

    Update follower.py

--- follower.py
+++ follower.py
@@ -1,50 +1,86 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import random
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-driver = webdriver.Firefox()
+web = webdriver.Firefox()
+web.get('https://www.instagram.com/accounts/login/')
+time.sleep(5)
 
-driver.get('https://www.instagram.com/accounts/login/')
+no_seguidos = 0
+ 
+ 
+def login():
+    usuario = web.find_element(By.XPATH,'/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[1]/div/label/input')
+    usuario.send_keys("Tu_cuenta_de_gmail")
 
-time.sleep(2)
-
-usuario = driver.find_element(By.XPATH,'/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[1]/div/label/input')
-usuario.send_keys("Tu correo o usuario de Instagram")
-
-password = driver.find_element(By.XPATH, "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[2]/div/label/input")
-password.send_keys("Tu password")
-
-
-login_boton = driver.find_element(By.XPATH, "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button/div")
-login_boton.click()
-
-time.sleep(4)
-
-driver.get('https://www.instagram.com/explore/people/')
-
-time.sleep(3)
+    password = web.find_element(By.XPATH, "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[2]/div/label/input")
+    password.send_keys("Tu_clave")
 
 
-def delay():
-    time.sleep(60)
-    driver.get('https://www.instagram.com/explore/people/')
-    time.sleep(3)
-    ciclo()
+    login_boton = web.find_element(By.XPATH, "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button/div")
+    login_boton.click()
+ 
+def open_followers(account_instagram):
+    web.get("https://www.instagram.com/" + account_instagram + "/followers/")
+    time.sleep(5)
+    web.find_element(By.XPATH,"/html/body/div[1]/section/main/div/header/section/ul/li[2]/a").click()
+ 
+ 
+def scroll_followers(minutes):
+    pop_up_window = web.find_element(By.XPATH,"/html/body/div[6]/div/div/div/div[2]")
+    #/html/body/div[6]/div/div/div[2] 
+    timeout = time.time() + 60 * minutes
+    print(f'-->{timeout}')
+    print(f'-->{time.time()}')
+    while True:
+        if time.time() > timeout:
+            break
+        web.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',pop_up_window)
+        time.sleep(1)
+ 
+ 
+def follow_followers():
+    #Variable global
+    global no_seguidos
 
-def ciclo():
-    for i in range(100):
-        boton_seguir = driver.find_element(By.XPATH, f"/html/body/div[1]/section/main/div/div[2]/div/div/div[{i+1}]/div[3]/button")
-        boton_seguir.click()
-        time.sleep(2)
-        #Aqui cada 6 cuentas que siga va tomar un descanso de un minuto
-        #puedes cambiarle 
-        if 5<i+1<7:
-           delay()
+    list_followers = web.find_element(By.XPATH,"/html/body/div[6]/div/div/div/div[2]/ul")
+    #/html/body/div[6]/div/div/div[2]/ul/div 
+    for child in list_followers.find_elements(By.CSS_SELECTOR,"li"):
+        user_name = child.find_element(By.CSS_SELECTOR,".notranslate").text
+        follow_button = child.find_element(By.CSS_SELECTOR,"button")
+        print(user_name)
+        print("----")
+        print(follow_button.text)
+        if "Follow" == follow_button.text:
+            follow_button.click()
+            time.sleep(2)
+            if "Follow" == follow_button.text:
+                no_seguidos = 0
+                print("Descanso")
+                time.sleep(120)
+            time.sleep(random.randint(1,3))
+            no_seguidos = no_seguidos + 1
+            print(f'N°{no_seguidos}')
+            print(user_name + "->seguido")
+            if(no_seguidos == 20):
+                no_seguidos = 0
+                time.sleep(120)
 
-try:
-    ciclo()
-except:
-    time.sleep(10)
-    ciclo()
+        else:
+            print("Ya lo sigues")
+        time.sleep(1)
+ 
+ 
+sources = ["Cuenta_a_las_que_deseas_seguir_sus_seguidores"]
+login()
+time.sleep(5)
+for account in sources:
+    open_followers(account)
+    time.sleep(5)
+    scroll_followers(10)
+    follow_followers()
+ 
+ 
